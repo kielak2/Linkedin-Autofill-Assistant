da5e8b88ffa0acd7bcee8faa66bb8f184ace04c5
kielak2 <maciek@kielak.com>
2025-01-16 08:45:24 +0100

    Refactor code

--- content.js
+++ content.js
@@ -1,46 +1,18 @@
-
-
-// Initialize the paste handler function
 document.addEventListener('paste', function (e)  {
 
-  let pastedText = (e.clipboardData || window.clipboardData).getData('text');
+  let pastedText = (e.clipboardData).getData('text');
 
   if (!pastedText.includes('{name}')) {
-    console.log('No {name} placeholder found in pasted text');
     return;
   }
 
-  const selectors = [
-    'h2.msg-entity-lockup__entity-title', 
-    '.profile-card-one-to-one__profile-link',
-  ];
-  let recipientName = ''; 
-
-  for (const selector of selectors) {
-    const element = document.querySelector(selector);
-    console.log(`Trying selector: ${selector}`, 'Element found:', element);
-
-    if (element) {
-      const fullName = element.textContent.trim();
-      console.log('Found full name:', fullName);
-
-      recipientName = fullName.split(' ')[0];
-      break;
-    }
-  }
-
+  const recipientName = getRecipientName();
   if (!recipientName) {
-    console.log('Could not find recipient name, paste proceeding normally');
     return;
   }
-
-  const modifiedText = pastedText.replace(/{name}/g, recipientName);
-
-  e.preventDefault();
 
   const activeElement = document.activeElement;
   if (activeElement && activeElement.classList.contains('msg-form__contenteditable')) {
-    console.log('Active chat box detected');
     const currentHTML = activeElement.innerHTML;
     const newHTML = currentHTML.replace('{name}', recipientName);
 
@@ -54,7 +26,25 @@ document.addEventListener('paste', function (e)  {
     selection.removeAllRanges();
     selection.addRange(range);
   } else {
-    console.log('No active chat box found');
   }
 }
 )
+
+function getRecipientName() {
+  const selectors = [
+    'h2.msg-entity-lockup__entity-title',
+    '.profile-card-one-to-one__profile-link',
+  ];
+
+  for (const selector of selectors) {
+    const element = document.querySelector(selector);
+
+    if (element) {
+      const fullName = element.textContent.trim();
+
+      return fullName.split(' ')[0];
+    }
+  }
+
+  return '';
+}
